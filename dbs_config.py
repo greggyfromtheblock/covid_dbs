3e8a09a130154c9aebea9f76cdc12dcbcc66c65a
Greg Ruyoga <gregruyoga@GregsMac.local>
2020-07-19 12:41:43 +0200

    commit init file & config

---
+++ dbs_config.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+from configparser import ConfigParser
+
+
+def config(filename='dbs.ini', section='postgresql'):
+
+    # create a parser
+    parser = ConfigParser()
+    # read config file
+    parser.read(filename)
+
+    # get section
+    db = {}
+
+    # Check if postgresql parser exists
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+
+    # return error if a param is called that is not listed in the init file
+    else raise Exception('Section {0} not fround in the {1} file'. format(section,filename))
+
+    return db
