ec9eb97fc55c0026432a670fd534d83ffcf8e5d8
ongakuaikoka <ongakuaikoka@gmail.com>
2020-07-28 12:51:45 +0200

    upd

--- db_connect.py
+++ db_connect.py
@@ -9,20 +9,13 @@ def connect():
     try:
         # connect to the PostgreSQL server
         print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(host="localhost", database="postgres", user="mytestuser", password="imt-mc-450")
+        conn = psycopg2.connect(host="localhost", database="postgres", user="mytestuser", password="qwerty")
 
         # create a cursor
         cur = conn.cursor()
 
-        # execute a statement
-        # print('PostgreSQL database version:')
-        # cur.execute('SELECT version()')
-
-        # display the PostgreSQL database server version
-        # db_version = cur.fetchone()
-        # print(db_version)
-        
-        # create a table with the user account data
+       
+        # create tables
         cur.execute("""CREATE TABLE COUNTRY(
                 countriesAndTerritories     TEXT    PRIMARY KEY     NOT NULL,
                 popData2018                 INT                             ,
@@ -30,19 +23,14 @@ def connect():
                 continentExp                TEXT                            );
                 """)
         
-        # insert info for 2 users
+        # insert info from files
         with open('Country.csv') as csvfile:
             country_data = csv.reader(csvfile)
             next(country_data, None)
             for row in country_data:
-                for i in row:
-                    if i == '':
-                        print (row.index(i))
-
-                        #row.index(i) = 0              
                 cur.execute('INSERT INTO COUNTRY(countriesAndTerritories, popData2018, countryTerritoryCode, continentExp)' 'VALUES(%s, %s, %s, %s)', row)
               
-        # give the list of usernames from the account table
+        # give the info for check
         cur.execute("""SELECT * FROM COUNTRY;""")
         query_results = cur.fetchall()
         print(query_results)
