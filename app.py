1fc5c9f203f7ca53f00fd5cc04d83b74380a2671
Greg Ruyoga <greg.pascalis@gmail.com>
2020-07-28 13:38:37 +0200

    add empty databases

--- app.py
+++ app.py
@@ -20,21 +20,47 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+class Countries(db.Model):
+    __tablename__ = 'countries'
+    cat = db.Column(db.String(200), primary_key=True)
+    popdata = db.Column(db.String(200))
+    ctc = db.Column(db.String(200))
+    continent_exp = db.Column(db.String(200))
+    def __init__(self, cat, popdata, ctc, continent_exp):
+        self.cat = cat
+        self.popdata = popdata
+        self.ctc = ctc
+        self.continent_exp = continent_exp
 
-class Feedback(db.Model):
-    __tablename__ = 'feedback'
-    id = db.Column(db.Integer, primary_key=True)
-    customer = db.Column(db.String(200), unique=True)
-    dealer = db.Column(db.String(200))
-    rating = db.Column(db.Integer)
-    comments = db.Column(db.Text())
+class Reports(db.Model):
+    __tablename__ = 'reports'
+    report_id = db.Column(db.Integer, primary_key=True)
+    geoID = db.Column(db.String(200))
+    day = db.Column(db.Integer)
+    month = db.Column(db.Integer)
+    year = db.Column(db.Integer)
+    cases = db.Column(db.Integer)
+    deaths = db.Column(db.Integer)
 
-    def __init__(self, customer, dealer, rating, comments):
-        self.customer = customer
-        self.dealer = dealer
-        self.rating = rating
-        self.comments = comments
+    def __init__(self, cat, popdata, ctc, continent_exp):
+        self.report_id = report_id
+        self.geoID = geoID
+        self.day = day
+        self.month = month
+        self.year = year
+        self.cases = cases
+        self.deaths = deaths
 
+class Reported(db.Model):
+    __tablename__ = 'reported'
+    dateRep = db.Column(db.String(200))
+    cat = db.Column(db.String(200), db.ForeignKey('countries.cat'))
+    report_id = db.Column(db.Integer, db.ForeignKey('reports.report_id'))
+
+    def __init__(self, cat, popdata, ctc, continent_exp):
+        self.dateRep = dateRep
+        self.cat = cat
+        self.report_id = report_id
 
 @app.route('/')
 def index():
