3d843af790356abe4d65fbb7d544509a88487433
Greg Ruyoga <greg.pascalis@gmail.com>
2020-07-28 12:51:51 +0200

    remove : from gunicorn

--- app.py
+++ app.py
@@ -42,24 +42,5 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    if request.method == 'POST':
-        customer = request.form['customer']
-        dealer = request.form['dealer']
-        rating = request.form['rating']
-        comments = request.form['comments']
-        # print(customer, dealer, rating, comments)
-        if customer == '' or dealer == '':
-            return render_template('index.html', message='Please enter required fields')
-        if db.session.query(Feedback).filter(Feedback.customer == customer).count() == 0:
-            data = Feedback(customer, dealer, rating, comments)
-            db.session.add(data)
-            db.session.commit()
-            send_mail(customer, dealer, rating, comments)
-            return render_template('success.html')
-        return render_template('index.html', message='You have already submitted feedback')
-
-
 if __name__ == '__main__':
     app.run()
