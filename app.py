a9e677c7a6df1b39bd44d2b064bb2b3ea1fbaab8
Greg Ruyoga <greg.pascalis@gmail.com>
2020-07-28 12:35:18 +0200

    test

--- app.py
+++ app.py
@@ -1,38 +1,65 @@
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import PrimaryKeyConstraint, create_engine
+
 import psycopg2
+from flask_sqlalchemy import SQLAlchemy
+from send_mail import send_mail
 from staging_createdb import createDB
 
 app = Flask(__name__)
 
-# ENV = 'dev'
-# if ENV == 'dev':
-#     address = 'postgresql://postgres:postgres@localhost/'
-#     app.debug = True
-#     app.config['SQLALCHEMY_DATABASE_URI'] = address
-#     db = SQLAlchemy(app)
-# else:
-#     address = 'postgres://swtsxkwlolegyh:ce7118b4964bf64170dba2e2aab740e2305ec8b69e572bf9d2d0f54381088bb4@ec2-54-159-138-67.compute-1.amazonaws.com:5432/d8uu6s9ng0kauk'
-#     app.debug = False
-#     app.config['SQLALCHEMY_DATABASE_URI'] = address
-#     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+ENV = 'prod'
 
-# conn_string = address
-# conn = psycopg2.connect(conn_string)
-# print('success')
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://wzeldjxjbxkkjk:615dffd940139b8c47cb0d0abbe40addda41b6d09b998e3a0253a67e34997c46@ec2-34-225-162-157.compute-1.amazonaws.com:5432/dc3rluetv2c0e8'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+    id = db.Column(db.Integer, primary_key=True)
+    customer = db.Column(db.String(200), unique=True)
+    dealer = db.Column(db.String(200))
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.Text())
+
+    def __init__(self, customer, dealer, rating, comments):
+        self.customer = customer
+        self.dealer = dealer
+        self.rating = rating
+        self.comments = comments
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# @app.route('/country/<name>', methods=['GET'])
-# def get_country(name):
-#     if request.method == 'GET':
-#         country = request.form['country']
-#     if country == '':
-#         return render_template('index.html', message='Please eneter required fields')
-#     return render_template('countries.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        customer = request.form['customer']
+        dealer = request.form['dealer']
+        rating = request.form['rating']
+        comments = request.form['comments']
+        # print(customer, dealer, rating, comments)
+        if customer == '' or dealer == '':
+            return render_template('index.html', message='Please enter required fields')
+        if db.session.query(Feedback).filter(Feedback.customer == customer).count() == 0:
+            data = Feedback(customer, dealer, rating, comments)
+            db.session.add(data)
+            db.session.commit()
+            send_mail(customer, dealer, rating, comments)
+            return render_template('success.html')
+        return render_template('index.html', message='You have already submitted feedback')
+
 
 if __name__ == '__main__':
     app.run()
