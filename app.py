801715231a8371bc619b34b5670da2262f141c88
Greg Ruyoga <greg.pascalis@gmail.com>
2020-08-04 10:23:10 +0200

    final

--- app.py
+++ app.py
@@ -7,12 +7,17 @@ from send_mail import send_mail
 from wtforms import Form, StringField, SelectField
 from db_setup import  init_db, db_session
 from flask_table import Table, Col
+from sqlalchemy import text
+from bokeh.models import ColumnDataSource, Grid, LinearAxis, Plot, VBar
+import pandas as pd
+from bokeh.plotting import figure, output_file, show, save, ColumnDataSource
+from bokeh.embed import components
 
 app = Flask(__name__)
 
 ENV = 'prod'
 
-if ENV == 'dev':
+if ENV == 'prod':
     app.debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/covid_staging'
     app.secret_key = 'gresziu'
@@ -46,7 +51,7 @@ class Countries(db.Model):
 class Reports(db.Model):
     __tablename__ = 'reports'
     report_id = db.Column(db.Integer, primary_key=True)
-    # cat = db.Column(db.String(200))
+    cat = db.Column(db.String(200))
     geo_id = db.Column(db.String(200))
     day = db.Column(db.Integer)
     month = db.Column(db.Integer)
@@ -85,10 +90,10 @@ class Results(Table):
   continent_exp = Col('Continent')
 
 class CasesAndDeaths(Table):
-    dateRep = Col('Date')
+    month = Col('Month')
+    day = Col('Day')
     cases = Col('Cases')
     deaths = Col('Deaths')
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -101,24 +106,51 @@ def index():
 def search_results(search):
     results = []
     search_string = search.data['search']
+
     countries_query = db_session.query(Countries).filter(Countries.cat == search_string)
-    cd_query = db_session.query(Reports).with_entities(Reported.dateRep, Reports.cases, Reports.deaths).filter(Reported.cat == search_string)
+    cd_query = db_session.query(Reports).with_entities(Reports.month, Reports.day, Reports.cases, Reports.deaths).filter(Reports.cat == search_string)
     cd = cd_query.all()
+    cd.reverse()
     results = countries_query.all()
 
-    # if search.data['search'] == '':
-    #     qry = db_session.query(Countries)
-    #     results = qry.all()
 
     if not results:
         flash('No results found!')
         return redirect('/')
     else:
+
         # display results
-        table = Results(results)
-        cd_table = Results(cd)
-        table.border = True
-        return render_template('results.html', table=cd)
+        info_table = Results(results)
+        cd_table = CasesAndDeaths(cd)
+        cd_table.border = True
+        info_table.border = True
+        df = pd.DataFrame(cd, columns=['month', 'day', 'cases', 'deaths'])
+        source = ColumnDataSource(df)
+
+        # Car list
+        day = df ['day']
+        cases = df['cases']
+        length = len(df)
+        print(length)
+        date_list = source.data['day'].tolist()
+        cases_list = source.data['cases'].tolist()
+        deaths_list = source.data['deaths'].tolist()
+
+        print(date_list)
+
+        p = figure(
+            plot_width=800,
+            plot_height=600,
+            title='COVID data for {search}'.format(search=search_string),
+            x_axis_label='Date',
+            tools="pan,box_select,zoom_in,zoom_out,save,reset"
+        )
+
+        glyph = VBar(x='day', top='cases', bottom=0, width=0.5, fill_color="#b3de69")
+        p.add_glyph(source, glyph)
+        script, div = components(p)
+        return render_template('results.html', table=info_table, div=div, script=script)
+
 
 if __name__ == '__main__':
     app.run()
--- populate_db.py
+++ populate_db.py
@@ -11,7 +11,6 @@ else:
     engine = create_engine('postgres://wzeldjxjbxkkjk:615dffd940139b8c47cb0d0abbe40addda41b6d09b998e3a0253a67e34997c46@ec2-34-225-162-157.compute-1.amazonaws.com:5432/dc3rluetv2c0e8')
 
 con = engine.connect()
-print(engine.table_names())
 
 def readCountries():
     print('Reading csv...')
@@ -22,23 +21,25 @@ def readCountries():
     for country in countries:
         fvi.append(df[df.countriesAndTerritories==country].first_valid_index())
     countrydf = df.loc[fvi,['countriesAndTerritories', 'popData2018',
-                            'countryterritoryCode', 'continentExp', 'geoId']]
-    countrydf.columns = ['cat', 'popdata', 'ctc', 'continent_exp', 'geo_id']
+                            'countryterritoryCode', 'continentExp']]
+    countrydf.columns = ['cat', 'popdata', 'ctc', 'continent_exp']
     return countrydf
 
 def readReported():
    print('Reading csv...')
    df = pd.read_csv('data/covid19.csv')
    reporteddf = df[['countriesAndTerritories', 'dateRep']]
-   reporteddf.columns = ['cat', 'dateRep']
+   serial = [x for x in range(1, 11182)]
+   reporteddf.insert(0, 'report_id', serial)
+   reporteddf.columns = ['report_id', 'cat', 'dateRep']
    return reporteddf
 
 def readReports():
     print('Reading csv...')
     df = pd.read_csv('data/covid19.csv')
-    reportsdf = df[['geoId', 'day', 'month', 'year',
+    reportsdf = df[['geoId', 'countriesAndTerritories', 'day', 'month', 'year',
                 'cases', 'deaths']]
-    reportsdf.columns = ['geo_id', 'day', 'month', 'year', 'cases', 'deaths']
+    reportsdf.columns = ['geo_id', 'cat', 'day', 'month', 'year', 'cases', 'deaths']
     return reportsdf
 
 countrydf = readCountries()
@@ -47,9 +48,11 @@ print(countrydf)
 
 reportsdf = readReports()
 reportsdf.to_sql('reports', con, if_exists = 'append', index=False)
+print(reportsdf)
 
 reporteddf = readReported()
-reporteddf.to_sql('reported', con, if_exists = 'append', index=True)
+reporteddf.to_sql('reported', con, if_exists = 'append', index=False)
+print(reporteddf)
 
 print('success biatch')
 
